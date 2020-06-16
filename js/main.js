45ef4e731fd48996603606abaca5b8893b5b7d5d
meji <soy@meji.es>
2020-06-16 12:30:52 +0200

    Slider y países primera parte

--- js/main.js
+++ js/main.js
@@ -18,3 +18,31 @@ document.addEventListener("DOMContentLoaded", function () {
   menu_mobile();
   menu_main();
 });
+
+//Slider de 4 - 3 - 2 - 1
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 6.5,
+  speed: 1000,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true
+  },
+  spaceBetween: 50,
+  // navigation: {
+  //   nextEl: '.swiper-button-next-4',
+  //   prevEl: '.swiper-button-prev-4'
+  // },
+  allowTouchMove: true,
+  breakpoints: {
+    1400: {
+      slidesPerView: 10.5
+    },
+    1024: {
+      slidesPerView: 6.5
+    },
+    468: {
+      slidesPerView: 3.7
+    }
+  }
+})
